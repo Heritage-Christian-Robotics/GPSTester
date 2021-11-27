7c106e67e7569fadb5f70721c18e8f362b2277fd
Caleb Buening <caleb.j.buening@gmail.com>
2021-11-27 13:18:52 -0500

    Fixed weird values + rotation replaced w/heading because rotation sucks

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,14 @@ void opcontrol() {
 	pros::lcd::initialize();
 	pros::lcd::print(0, "GPS Tester");
 	pros::lcd::print(1, "------------------------");
+	gps.initialize_full(0, 0, 0, 0, 0);
 
 	while (true) {
 		// Render data
-		pros::lcd::print(2, "rot:%4.2d", gps.get_rotation());
-		pros::lcd::print(3, "locx:%4.2d", gps.get_status().x);
-		pros::lcd::print(4, "locy:%4.2d", gps.get_status().y);
-		pros::lcd::print(5, "error:%4.2d", gps.get_error());
+		pros::lcd::print(2, "rot:%4.2f", gps.get_heading());
+		pros::lcd::print(3, "locx:%4.2f", gps.get_status().x);
+		pros::lcd::print(4, "locy:%4.2f", gps.get_status().y);
+		pros::lcd::print(5, "error:%4.2f", gps.get_error());
 		pros::delay(10);
 	}
 }
